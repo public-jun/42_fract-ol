260b742cac85708ef04e3a90a410fad4633fc1ad
jnakahod <jnakahod@student.42tokyo.jp>
2021-06-22 04:37:07 +0200

    test

--- srcs/main.c
+++ srcs/main.c
@@ -12,13 +12,34 @@
 
 #include <fractol.h>
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	data;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World");
+	data.img = mlx_new_image(mlx, 1920, 1080);
+	data.addr = mlx_get_data_addr(data.img, &data.bpp, &data.line_length, &data.endian);
+	my_mlx_pixel_put(&data, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
 	mlx_loop(mlx);
-	return (0);
 }
