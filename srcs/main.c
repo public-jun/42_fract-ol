18928def2fc28aa0c8a2d0b690b0d8add16a53c6
public-jun <generalpublicjunjun@gmail.com>
2021-07-12 22:35:20 +0900

    norm main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 22:31:05 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/07/12 22:19:24 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/07/12 22:35:01 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
-void branch_by_fractol(int x, int y, t_all *all)
+void	branch_by_fractol(int x, int y, t_all *all)
 {
 	if (all->type_fractol == MANDELBROT)
 		set_pixel_mandelbrot(x, y, all);
@@ -22,10 +22,10 @@ void branch_by_fractol(int x, int y, t_all *all)
 		set_pixel_burningship(x, y, all);
 }
 
-void ft_draw_to_window(t_all *all, t_data *data)
+void	ft_draw_to_window(t_all *all, t_data *data)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	y = 0;
 	while (y < HEIGHT)
@@ -34,7 +34,8 @@ void ft_draw_to_window(t_all *all, t_data *data)
 		while (x < WIDTH)
 		{
 			branch_by_fractol(x, y, all);
-			data->addr[(data->size_l / (data->bpp / 8)) * y + x] = all->buf[y][x];
+			data->addr[(data->size_l / (data->bpp / 8)) *y + x]
+				= all->buf[y][x];
 			x++;
 		}
 		y++;
@@ -42,15 +43,15 @@ void ft_draw_to_window(t_all *all, t_data *data)
 	mlx_put_image_to_window(all->mlx, all->win, data->img, 0, 0);
 }
 
-int ft_main_loop(t_all *all)
+int	ft_main_loop(t_all *all)
 {
 	ft_draw_to_window(all, &all->data);
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_all all;
+	t_all	all;
 
 	load_info_from_parameters(ac, av, &all);
 	ft_init(&all);
@@ -60,10 +61,10 @@ int main(int ac, char **av)
 	mlx_hook(all.win, 33, 1L << 17, &ft_exit, &all);
 	if (all.type_fractol == JULIA && all.flag_cursor_effect == ON)
 		mlx_hook(all.win, MOTIONNOTIFY, 1L << 6,
-				 &ft_update_constant_of_julia, &all);
+			 &ft_update_constant_of_julia, &all);
 	if (all.type_fractol == MANDELBROT && all.flag_cursor_effect == ON)
 		mlx_hook(all.win, MOTIONNOTIFY, 1L << 6,
-				 &ft_update_color_of_mandelbrot, &all);
+			 &ft_update_color_of_mandelbrot, &all);
 	mlx_loop(all.mlx);
 	return (0);
 }
